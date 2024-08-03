c068c27148488f3c923e1cd4af5a81ba269799a8
REENAGM <156776689+REENAGM@users.noreply.github.com>
2024-08-03 21:56:27 +0300

    Add files via upload

---
+++ count_days_in_week.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+
+	const int COUNT_DAYS_IN_WEEK = 7;
+    const char* NEW_LINE = "\n";
+	const char* TAB = "\t";
+	cout << COUNT_DAYS_IN_WEEK << NEW_LINE << TAB;
+	
+	return 0;
+
+
+
+
+}
